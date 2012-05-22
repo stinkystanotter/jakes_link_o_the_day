565c43991c494ed6c8602b1e59ee26ae64a3b067
Jacob Vass <jacobvass@gmail.com>
2012-05-22 12:46:53 -0500

    one change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JakesLinkOTheDay::Application.config.secret_token = '85bffab93d768f5636b52ef2c98a42fd706c2f4d19b69df027a22b3d8b769f2f81ac20a668210fe7a22fc34606c54db55770f1637832c888f607c92d845a4650'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JakesLinkOTheDay::Application.config.session_store :cookie_store, key: '_jakes_link_o_the_day_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JakesLinkOTheDay::Application.config.session_store :active_record_store
