b8422f088762f2dcf7331e9cb0c094c6f9387db4
Jacob Vass <jacobvass@gmail.com>
2012-05-24 16:12:08 -0500

    added all the things

---
+++ db/migrate/20120522180154_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :field
+      t.link :
+      t.string :
+
+      t.timestamps
+    end
+  end
+end
